89b137842feafd9dd44782b86df63974ed369d68
gabriel <soadhenri@gmail.com>
2020-12-06 17:19:58 -0300

    tela 'cadastro' criada

--- financasapp/src/pages/SignUp/index.js
+++ financasapp/src/pages/SignUp/index.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { Platform, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
-import { Background, Container, AreaInput, Logo, Input, SubmitButton, SubmitText, Link, LinkText } from './styles';
+
+import { Background, Container, AreaInput, Logo, Input, SubmitButton, SubmitText } from '../SignIn/styles';
 
 export default SignIn = () => {
-    
-    const navigation = useNavigation();
 
+    const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -17,11 +16,19 @@ export default SignIn = () => {
             behavior={Platform.OS === 'ios' ? 'padding' : ''}
             enabled
             >
-                <Logo source={require('../../assets/Logo.png')}/>
 
                 <AreaInput>
                     <Input 
-                    placeholder='Password'
+                    placeholder='Nome'
+                    autpCorrect={false}
+                    autoCapitalize='none'
+                    value={nome}
+                    onChangeText={text => setNome(text)}
+                    />
+                </AreaInput>
+                <AreaInput>
+                    <Input 
+                    placeholder='Senha'
                     autpCorrect={false}
                     autoCapitalize='none'
                     value={email}
@@ -37,13 +44,10 @@ export default SignIn = () => {
                     onChangeText={text => setPassword(text)}
                     />
                 </AreaInput>
-                <SubmitButton>
-                    <SubmitText>Acessar</SubmitText>
-                </SubmitButton>
 
-                <Link onPress={ () => navigation.navigate('SignUp') }>
-                    <LinkText>Criar uma conta</LinkText>
-                </Link>
+                <SubmitButton>
+                    <SubmitText>Cadastrarr</SubmitText>
+                </SubmitButton>
 
             </Container>
         </Background>
