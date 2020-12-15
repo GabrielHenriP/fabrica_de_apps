12ce92784f59baf13966e264e04bdbff29a5d3f9
gabriel <soadhenri@gmail.com>
2020-12-15 17:37:44 -0300

    navigation Drawer done

---
+++ financasapp/src/pages/New/index.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
+
+export default New = () => {
+
+    return(
+        <View>
+            <Text>New</Text>
+        </View>
+    )
+};
