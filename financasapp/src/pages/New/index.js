86a91460e9ed88bde1667517d7461beed4665d82
gabriel <soadhenri@gmail.com>
2020-12-18 16:33:55 -0300

    screen 'Registrat' done

--- financasapp/src/pages/New/index.js
+++ financasapp/src/pages/New/index.js
@@ -1,11 +1,39 @@
-import React, { useContext } from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { TouchableWithoutFeedback, SafeAreaView, Keyboard } from 'react-native';
+
+import HeaderMenu from '../../components/Header';
+
+import { Background, Input, SubmitButton, SubmitText} from './styles';
+import Picker from '../../components/Picker';
 
 export default New = () => {
 
+    const [valor, SetValor] = useState('');
+    const [tipo, setTipo] = useState('receita')
+
     return(
-        <View>
-            <Text>New</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Background>
+                <HeaderMenu/>
+
+                <SafeAreaView style={{alignItems:'center'}}>
+                    <Input
+                    placeholder='Valor desejado'
+                    keyboardType='numeric'
+                    returnKeyType='next'
+                    onSubmitEditing={ () => Keyboard.dismiss()}
+                    value={valor}
+                    onChangeText={ text => SetValor(text)}
+                    />
+
+                    <Picker onChange={setTipo} tipo={tipo}/>
+
+                    <SubmitButton>
+                        <SubmitText>Registrar</SubmitText>
+                    </SubmitButton>
+
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
     )
 };
