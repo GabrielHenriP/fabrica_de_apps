e8c7f31ed371f0f0ca823ef2679dbf962864098e
gabriel <soadhenri@gmail.com>
2020-12-19 11:47:39 -0300

    recording transctions with firebase

--- financasapp/src/components/Picker/index.android.js
+++ financasapp/src/components/Picker/index.android.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Picker as RNPickerSelect } from '@react-native-picker/picker';
+
 
 import {PickerView} from './styles';
 
-export default Picker = ({ onChange, tipo }) => {
+export default Picker = ({ onChange, tipo, valor:val }) => {
+
+    const [select, setSelect] = useState('')
+    console.log(select)
     return(
         <PickerView>
             <RNPickerSelect
             style={{
                 width:'100%'
             }}
-            selectedValue={tipo}
-            onValueChange={ valor => onChange(valor) }
+            selectedValue={val === '' ? '' : select}
+            onValueChange={ valor => {onChange(valor); setSelect(valor)} }
             >
-                <RNPickerSelect.Item label='Receita' value='receita' />
-                <RNPickerSelect.Item label='Despesa' value='despesa' />
+                <RNPickerSelect.Item label='Selecione o tipo' color='#ededed'/>
+                <RNPickerSelect.Item label='Receita' value='receita' color='balck'/>
+                <RNPickerSelect.Item label='Despesa' value='despesa' color='balck' />
             </RNPickerSelect>
         </PickerView>
     )
--- financasapp/src/pages/New/index.js
+++ financasapp/src/pages/New/index.js
@@ -1,5 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { TouchableWithoutFeedback, SafeAreaView, Keyboard } from 'react-native';
+import { TouchableWithoutFeedback, SafeAreaView, Keyboard, Alert } from 'react-native';
+import { format } from 'date-fns';
+import { useNavigation } from '@react-navigation/native';
+import firebase from '../../services/firebaseConnection';
+import { AuthContext } from '../../contexts/auth';
 
 import HeaderMenu from '../../components/Header';
 
@@ -8,8 +12,59 @@ import Picker from '../../components/Picker';
 
 export default New = () => {
 
+    const { user:usuario } = useContext(AuthContext);
+
+    const navigation = useNavigation();
+
     const [valor, SetValor] = useState('');
     const [tipo, setTipo] = useState('receita')
+
+    function handleSubmit(){
+        Keyboard.dismiss()
+        if(isNaN(parseFloat(valor)) || tipo == null){
+            alert('Preencha todos os campos')
+            return;
+        }   
+        Alert.alert(
+            'Confirmando dados',
+            `Tipo ${tipo} - valor: ${parseFloat(valor)}`,
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => handleAdd()
+                }
+            ]
+        )
+    }
+
+    async function handleAdd(){
+        let uid = usuario.uid;
+
+        let key = await firebase.database().ref('historico').child(uid).push().key;
+
+        await firebase.database().ref('historico').child(uid).child(key).set({
+            tipo:tipo,
+            valor:parseFloat(valor),
+            date: format(new Date(), 'dd/MM/yy')
+        })
+        //atualizar o nosso saldo
+        let user = firebase.database().ref('users').child(uid);
+        await user.once('value').then( snapshot => {
+            let saldo = parseFloat(snapshot.val().saldo)
+
+            tipo === 'despesa' ? saldo -= parseFloat(valor): saldo += parseFloat(valor)
+
+            user.child('saldo').set(saldo)
+
+        })
+        SetValor('')
+        Keyboard.dismiss()
+        navigation.navigate('Home')
+    }
 
     return(
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -26,9 +81,9 @@ export default New = () => {
                     onChangeText={ text => SetValor(text)}
                     />
 
-                    <Picker onChange={setTipo} tipo={tipo}/>
+                    <Picker onChange={setTipo} select={valor}/>
 
-                    <SubmitButton>
+                    <SubmitButton onPress={handleSubmit}>
                         <SubmitText>Registrar</SubmitText>
                     </SubmitButton>
 
